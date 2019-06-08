78d23830fbde71f93cca10a09a17da4f5f873f44
richardjpurcell <richardjpurcell@yahoo.com>
2019-06-08 15:34:15 -0300

    [RP]-3 Added info pages
    Added info pages and button functionality.

--- app/src/main/java/com/example/mydrugpal/MainActivity.java
+++ app/src/main/java/com/example/mydrugpal/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.mydrugpal;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToHeisenberg(View view) {
+        Intent intent = new Intent(this, Heisenberg.class);
+        startActivity(intent);
+    }
+
+    public void goToWhiskey(View view) {
+        Intent intent = new Intent(this, Whiskey.class);
+        startActivity(intent);
+    }
+
+    public void goToCannabis(View view) {
+        Intent intent = new Intent(this, Cannabis.class);
+        startActivity(intent);
+    }
+
+    public void goToAcid(View view) {
+        Intent intent = new Intent(this, Acid.class);
+        startActivity(intent);
+    }
+
+    public void goToCocaCola(View view) {
+        Intent intent = new Intent(this, CocaCola.class);
+        startActivity(intent);
     }
 }
