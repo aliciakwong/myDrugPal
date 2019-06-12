04586d9a70a1ae2a07f867f389744b632f816c83
Emma Travers <em891299@dal.ca>
2019-06-12 14:25:33 -0300

    [ET-RP] #3 - Attempting to access string in Description field in Substances collection (wip)

--- app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
+++ app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
@@ -2,15 +2,24 @@ package com.example.mydrugpal;
 
 import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 //import com.example.mydrugpal.model.Substances.Acid;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firestore.v1.StructuredQuery;
 
 public class AcidInfoActivity extends AppCompatActivity {
 
@@ -19,7 +28,11 @@ public class AcidInfoActivity extends AppCompatActivity {
     CollectionReference sub_collection = database.collection("Substances");
 
     DocumentReference acid = sub_collection.document("Cannabis");
+    //DocumentSnapshot acid_snap = acid.addSnapshotListener(new DocumentSnapshot());
+    //StructuredQuery.FieldReference sub_field = acid.get
 
+
+    //acid.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>);
     private Intent intent;
 
     private TextView acidDesc;
@@ -35,8 +48,17 @@ public class AcidInfoActivity extends AppCompatActivity {
 
         intent = getIntent();
 
+        sub_collection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
 
-        acidDesc.setText(acid.get().toString());
+            @Override
+            //need to put a method which will set the text of the Acid TextView
+
+
+        });
+
+        //need to get field text
+
+        acidDesc.setText(acid.getId());
 
 
     }
