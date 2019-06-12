f90112abc85f0d3ef91d8b5c774e238c77a3c273
Jocelyn MacDonald <jc210282@dal.ca>
2019-06-12 13:39:20 -0300

    [JM MB] [UI] #1 -  added espresso test to test registration page with register and clear functions

--- app/src/androidTest/java/com/example/mydrugpal/RegistrationInstrumentedTests.java
+++ app/src/androidTest/java/com/example/mydrugpal/RegistrationInstrumentedTests.java
@@ -1,6 +1,53 @@
 package com.example.mydrugpal;
 
-public class RegistrationInstrumentedTests
-{
+import android.preference.EditTextPreference;
+import android.widget.EditText;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+
+public class RegistrationInstrumentedTests {
+    @Rule
+    public ActivityTestRule<RegistrationActivity> activityRule
+            = new ActivityTestRule<>(RegistrationActivity.class);
+
+
+    @Test
+    public void appOpensRegistration(){
+        onView(withId(R.id.textView_fName)).toString().equals("First name");
+        onView(withId(R.id.textView_lName)).toString().equals("Last name");
+        onView(withId(R.id.textView_Email)).toString().equals("Email");
+        onView(withId(R.id.textView_Password)).toString().equals("Password");
+
+    }
+    @Test
+    public void testRegister() {
+        onView(withId(R.id.editText_fName)).perform(typeText("name"), closeSoftKeyboard());
+        onView(withId(R.id.editText_lName)).perform(typeText("lastname"), closeSoftKeyboard());
+        onView(withId(R.id.editText_Email)).perform(typeText("email"), closeSoftKeyboard());
+        onView(withId(R.id.editText_Password)).perform(typeText("password"), closeSoftKeyboard());
+        onView(withId(R.id.register_button)).perform(click());
+    }
+    @Test
+    public void testClearButton() {
+        onView(withId(R.id.editText_fName)).perform(typeText("name"), closeSoftKeyboard());
+        onView(withId(R.id.editText_lName)).perform(typeText("lastname"), closeSoftKeyboard());
+        onView(withId(R.id.editText_Email)).perform(typeText("email"), closeSoftKeyboard());
+        onView(withId(R.id.editText_Password)).perform(typeText("password"), closeSoftKeyboard());
+        onView(withId(R.id.clear_button)).perform(click());
+        onView(withId(R.id.textView_fName)).toString().equals("");
+        onView(withId(R.id.textView_lName)).toString().equals("");
+        onView(withId(R.id.textView_Email)).toString().equals("");
+        onView(withId(R.id.textView_Password)).toString().equals("");
+    }
+
 
 }
