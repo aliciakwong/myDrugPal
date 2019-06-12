e4dbf3d5cb6f39ca27bad4d5665bf80f5f40bc5c
Emma Travers <em891299@dal.ca>
2019-06-12 12:53:24 -0300

    [ET-RP] #3 - added some updates to AcidDetails for FireBase, commiting now to merge from master for firebase updates.

--- app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
+++ app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
@@ -1,14 +1,41 @@
 package com.example.mydrugpal;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.mydrugpal.model.Substance;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 
 public class AcidInfoActivity extends AppCompatActivity {
+
+
+    FireBaseFirestore database;
+    private Intent intent;
+    private Substance substance;
+
+    private TextView acidDesc;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acid);
+
+
+        database = FirebaseFirestore.getInstance();
+
+        intent = getIntent();
+
+        substance = (Substance)intent.getSerializableExtra("substance");
+
+        acidDesc = findViewByID(R.id.textViewAcid);
+        //acidDesc.setText(substance.cannabis.)
+
     }
 }
