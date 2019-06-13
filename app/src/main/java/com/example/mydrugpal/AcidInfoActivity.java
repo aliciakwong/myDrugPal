d13aa09008d9229d000a61ff09066a73435663e0
Emma Travers <em891299@dal.ca>
2019-06-13 11:36:43 -0300

    [ET-RP] #3 - WIP attempting to access Description field with onCompleteListener method

--- app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
+++ app/src/main/java/com/example/mydrugpal/AcidInfoActivity.java
@@ -1,6 +1,8 @@
 package com.example.mydrugpal;
 
 import android.content.Intent;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
@@ -13,52 +15,66 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 //import com.example.mydrugpal.model.Substances.Acid;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firestore.v1.Document;
 import com.google.firestore.v1.StructuredQuery;
+
+import java.util.List;
 
 public class AcidInfoActivity extends AppCompatActivity {
 
 
-    FirebaseFirestore database = FirebaseFirestore.getInstance();
-    CollectionReference sub_collection = database.collection("Substances");
 
-    DocumentReference acid = sub_collection.document("Cannabis");
-    //DocumentSnapshot acid_snap = acid.addSnapshotListener(new DocumentSnapshot());
-    //StructuredQuery.FieldReference sub_field = acid.get
-
-
-    //acid.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>);
     private Intent intent;
 
     private TextView acidDesc;
-
+    public String field_text = "blah";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acid);
 
-        acidDesc = findViewById(R.id.textViewAcid);
-        database = FirebaseFirestore.getInstance();
+        FirebaseFirestore database = FirebaseFirestore.getInstance();
+        CollectionReference sub_collection = database.collection("Substances");
 
-        intent = getIntent();
+        DocumentReference acid = sub_collection.document("Cannabis");
 
-        sub_collection.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>(){
 
+        acid.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
-            //need to put a method which will set the text of the Acid TextView
+            public void onComplete(@NonNull Task<DocumentSnapshotSnapshot> task) {
+                if (task.isSuccessful()) {
+                    //field_text = "task successful";
+                    //List<DocumentSnapshot> sList = task.getResult().getDocuments();
+                    //field_text = sList.get(1).getString("Description");
 
-
+                    DocumentSnapshot a_snap = task.getResult();
+                    if (a_snap != null) {
+                        field_text = a_snap.get("Description");
+                    }
+                    else {
+                        field_text = "it's not working";
+                    }
+                }
+            }
         });
 
-        //need to get field text
+        acidDesc = findViewById(R.id.textViewAcid);
+        //intent = getIntent();
 
-        acidDesc.setText(acid.getId());
+        //need to get field text
+        //String field = acid_snap.get("Description").toString();
+        acidDesc.setText(field_text); //currently returns document id "cannabis"
 
 
     }
