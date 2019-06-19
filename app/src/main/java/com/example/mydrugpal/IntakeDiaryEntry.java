3eb51e3d01c2f01b6ae9d18f890788c3d5019408
jocelyn <jc210282@dal.ca>
2019-06-19 14:16:38 -0300

    [JM IS] [UNIT] - #5 setup intake diary entry class with unit test

---
+++ app/src/main/java/com/example/mydrugpal/IntakeDiaryEntry.java
@@ -0,0 +1,54 @@
+package com.example.mydrugpal;
+
+import com.google.firebase.Timestamp;
+
+import java.io.Serializable;
+
+
+/**
+ *holds the data for an entry to the intake diary
+ * @author Jocelyn MacDonald, Ian Sifton
+ */
+public class IntakeDiaryEntry implements Serializable {
+    public String nameOfSubstance;
+    public String typeOfSubstance;
+    public int amount;
+    public Timestamp time;
+
+    /**
+     * The constructor for the IntakeDiaryEntry class
+     * @param nameOfSubstance the name of the substance being added
+     * @param typeOfSubstance the type of the substance being added
+     * @param amount the amount of the substance taken
+     */
+    public IntakeDiaryEntry(String nameOfSubstance, String typeOfSubstance, int amount) {
+        this.nameOfSubstance = nameOfSubstance;
+        this.typeOfSubstance = typeOfSubstance;
+        this.amount = amount;
+        time = Timestamp.now();
+    }
+
+    /**
+     * gets the name of the substance
+     * @return returns the name of the substance
+     */
+    public String getNameOfSubstance() {return nameOfSubstance;}
+
+    /**
+     * gets the type of the substance
+     * @return returns the type of substance
+     */
+    public String getTypeOfSubstance() {return typeOfSubstance;}
+
+    /**
+     * gets the amount of the substance logged
+     * @return returns the amount
+     */
+    public int getAmount() {return amount;}
+
+    /**
+     * gets the time of the entry
+     * @return returns the time of entry
+     */
+    public Timestamp getTime() {return time;}
+}
---
+++ app/src/test/java/com/example/mydrugpal/IntakeDiaryEntryUnitTest.java
@@ -0,0 +1,58 @@
+package com.example.mydrugpal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * unit test for the IntakeDiaryEntry class
+ * @author Jocelyn MacDonald, Ian Sifton
+ */
+
+public class IntakeDiaryEntryUnitTest {
+
+    /**
+     * tests that the entry is not null
+     */
+    @Test
+    public void intakeEntry_NotEmpty()
+    {
+        IntakeDiaryEntry entry = new IntakeDiaryEntry("fentanyl", "narcotic", 3);
+
+        assertNotNull(entry.getNameOfSubstance());
+        assertNotNull(entry.getTypeOfSubstance());
+        assertNotNull(entry.getAmount());
+        assertNotNull(entry.getTime());
+    }
+
+    /**
+     * tests that the type of the substance was set correctly
+     */
+    @Test
+    public void getNameOfSubstance() {
+        IntakeDiaryEntry entry = new IntakeDiaryEntry("fentanyl", "narcotic", 3);
+
+        assertEquals(entry.getNameOfSubstance(), "fentanyl");
+    }
+
+
+    /**
+     * tests that the amount of the substance was set correctly
+     */
+    @Test
+    public void getAmount() {
+        IntakeDiaryEntry entry = new IntakeDiaryEntry("fentanyl", "narcotic", 3);
+
+        assertEquals(entry.getAmount(), 3);
+    }
+
+    /**
+     * tests the timestamp was set and is not null
+     */
+    @Test
+    public void getTypeOfSubstance() {
+        IntakeDiaryEntry entry = new IntakeDiaryEntry("fentanyl", "narcotic", 3);
+
+        assertEquals(entry.getTypeOfSubstance(), "narcotic");
+    }
+}
