567a220481370d5cd1917755d98326564590511b
Emma Travers <em891299@dal.ca>
2019-06-23 14:46:09 -0300

    [ET AW] [UI] #22 - Created test for logout button

---
+++ app/src/androidTest/java/com/example/mydrugpal/LogoutInstrumentedTest.java
@@ -0,0 +1,36 @@
+package com.example.mydrugpal;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+
+/**
+ * JUnit Tests for logout page business logic
+ * @author Emma Travers, Alicia Wong
+ */
+public class LogoutInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<LogoutActivity> activityRule
+            = new ActivityTestRule<>(LogoutActivity.class);
+
+    /**
+     * test to check that once a user clicks the Logout button, they are returned to the Login page
+     */
+    @Test
+    public void logoutTransition() {
+
+        onView(withId(R.id.button_logout)).perform(click());
+        onView(withId(R.id.Title_MyDrugPal)).toString().equals("MyDrugPal");
+
+    }
+
+
+
+}
