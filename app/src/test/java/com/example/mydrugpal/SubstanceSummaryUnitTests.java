1f764b700d3dfed5d1f74e800d6cabe8443f5d5e
Megan Brock <mg247961@dal.ca>
2019-06-30 18:08:40 -0300

    [MB AW] [UNIT] #7 Added unit tests for substance summary information

--- app/src/test/java/com/example/mydrugpal/SubstanceSummaryUnitTests.java
+++ app/src/test/java/com/example/mydrugpal/SubstanceSummaryUnitTests.java
@@ -3,6 +3,8 @@ package com.example.mydrugpal;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 /**
  * Tests the SubstanceSummaryActivity and SubstanceSummaryInformation class
  *
@@ -19,5 +21,21 @@ public class SubstanceSummaryUnitTests
         SubstanceSummaryInformation info = new SubstanceSummaryInformation();
 
         assertNotNull(info.getSubstanceList());
+    }
+
+    /**
+     * Tests that both date parsing functions result
+     * in same date for same input values of different type.
+     */
+    @Test
+    public void TestParseDate()
+    {
+        String d1 = "2019-07-12T12:34:25-0700";
+        int[] d2 = {2019, 7, 12};
+
+        Date date1 = SubstanceSummaryInformation.parseDate(d1);
+        Date date2 = SubstanceSummaryInformation.parseDate(d2);
+
+        assertEquals(date1.getTime(), date2.getTime());
     }
 }
