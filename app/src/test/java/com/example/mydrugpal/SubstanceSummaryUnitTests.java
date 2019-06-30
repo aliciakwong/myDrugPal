b0034497b7f8fd5239544ace23f30c963ecee759
Megan Brock <mg247961@dal.ca>
2019-06-30 15:23:05 -0300

    [MB AW] [UNIT] #7 Added unit tests for substance summary information

--- app/src/test/java/com/example/mydrugpal/SubstanceSummaryUnitTests.java
+++ app/src/test/java/com/example/mydrugpal/SubstanceSummaryUnitTests.java
@@ -1,12 +1,23 @@
 package com.example.mydrugpal;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+/**
+ * Tests the SubstanceSummaryActivity and SubstanceSummaryInformation class
+ *
+ * @author Megan Brock, Alicia Wong
+ */
 public class SubstanceSummaryUnitTests
 {
+    /**
+     * Tests that the summary information returns
+     */
     @Test
-    public void TestSubstanceSummary()
+    public void TestSubstanceSummaryInformation()
     {
+        SubstanceSummaryInformation info = new SubstanceSummaryInformation();
 
+        assertNotNull(info.getSubstanceList());
     }
 }
