17ecda29522bf1b4c459789127a02b2100c92f22
richardjpurcell <richardjpurcell@yahoo.com>
2019-07-03 12:59:26 -0300

    [RP AW][UI] #21-Instrumented Tests Created
    Added instrumented tests based on user story.

---
+++ app/src/androidTest/java/com/example/mydrugpal/AboutAppInstrumentedTests.java
@@ -0,0 +1,55 @@
+package com.example.mydrugpal;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+
+/**
+ * Tests the AboutApp UI/functionality
+ *
+ * @author Alicia Wong, Richard Purcell
+ */
+
+public class AboutAppInstrumentedTests {
+
+    /**
+     * Rule for the registration activity tests (needs activity page created)
+     *
+     */
+
+    //TODO: add the initial ActivityTestRule
+
+    /**
+     * Tests that the go to AboutApp button opens in aboutApp information page
+     * and displays correct information.
+     */
+    @Test
+    public void appOpensAboutApp(){
+        //TODO: add the logic
+    }
+
+    /**
+     * Tests that that a go to company web page is clickable.
+     */
+    @Test
+    public void goToCompanyWebsite(){
+        //TODO: add the logic
+    }
+
+    /**
+     * Tests that that an email info button is active in some way.
+     */
+    @Test
+    public void clickEmail(){
+        //TODO: add the logic
+    }
+
+
+}
