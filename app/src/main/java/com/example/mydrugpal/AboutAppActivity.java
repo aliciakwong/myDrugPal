30151f10d2bea73759a0ec54acfbf72b01cfef9c
richardjpurcell <richardjpurcell@yahoo.com>
2019-07-07 14:40:14 -0300

    [RP AW][UNIT] #21-Added AboutAppActivity page

---
+++ app/src/main/java/com/example/mydrugpal/AboutAppActivity.java
@@ -0,0 +1,19 @@
+package com.example.mydrugpal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * activity with info about the app and the dev team
+ * @author Alicia Wong, Richard Purcell
+ */
+
+public class AboutAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+    }
+}
