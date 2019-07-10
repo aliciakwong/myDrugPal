055b41ce3846d0c6c275abe60db2263ba39dedb2
richardjpurcell <richardjpurcell@yahoo.com>
2019-07-10 14:07:38 -0300

    [RP IS][UI] #10-Began roughing in logic for notification alarm

--- app/src/main/java/com/example/mydrugpal/LogIntakeNotificationActivity.java
+++ app/src/main/java/com/example/mydrugpal/LogIntakeNotificationActivity.java
@@ -1,8 +1,20 @@
 package com.example.mydrugpal;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LogIntakeNotificationActivity extends AppCompatActivity {
 
+    private AlarmManager alarmMgr;
+    private PendingIntent alarmIntent;
+
+    Context context = this;
+
+    //alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE)
 
 }
